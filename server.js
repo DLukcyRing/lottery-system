3d5dd6787bd75e2bc4d42408065ae8489f944cdc
unknown <zhaoyaoyi@kose.com.cn>
2026-05-13 12:05:59 +0800

    Initial commit: 抽奖系统项目

---
+++ server.js
@@ -0,0 +1,15 @@
+﻿const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, function() {
+  console.log('Server running on http://localhost:' + port);
+});
